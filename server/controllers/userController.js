a30eb044be128ffb6e1efdbcd2f0e0fb8e5d6584
RituChoudhary01 <ritu9306541427@gmail.com>
2025-07-12 10:00:24 +0530

    main

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,12 @@
+import {Webhook} from 'svix'
 
 // API Controller Function to manage Clerk User with database
 // https://localhost:4000/api/user/webhooks
 const clerkwebhooks = async(req,res)=>{
-
+try{
+  // create a svix instance with clerk webhook secret.
+  const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+}catch(error){
+  
+}
 }
